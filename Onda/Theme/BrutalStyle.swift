91a14bcc307402877e1437fd316396d24fcbde0d
Matthew Gilliam <chase.gilliam@gmail.com>
2026-07-16 14:47:01 -0400

    feat: brutal style view modifiers (border, hard shadow, header, card)

---
+++ Onda/Theme/BrutalStyle.swift
@@ -0,0 +1,47 @@
+//  BrutalStyle.swift
+import SwiftUI
+
+private struct BrutalBorder: ViewModifier {
+    @Environment(AppTheme.self) private var theme
+    var width: CGFloat
+    func body(content: Content) -> some View {
+        content.overlay(Rectangle().stroke(theme.color(.border), lineWidth: width))
+    }
+}
+
+private struct HardShadow: ViewModifier {
+    @Environment(AppTheme.self) private var theme
+    var offset: CGFloat
+    func body(content: Content) -> some View {
+        content.background(
+            Rectangle().fill(theme.color(.cardShadow))
+                .offset(x: offset, y: offset)
+        )
+    }
+}
+
+extension View {
+    func brutalBorder(width: CGFloat = 2.5) -> some View { modifier(BrutalBorder(width: width)) }
+    func hardShadow(offset: CGFloat = 4) -> some View { modifier(HardShadow(offset: offset)) }
+}
+
+extension Text {
+    func brutalHeader(size: CGFloat) -> some View {
+        self.font(.system(size: size, weight: .black, design: .default))
+            .textCase(.uppercase)
+            .tracking(-0.5)
+    }
+}
+
+struct BrutalCard<Content: View>: View {
+    @Environment(AppTheme.self) private var theme
+    var offset: CGFloat = 4
+    var borderWidth: CGFloat = 2.5
+    @ViewBuilder var content: Content
+    var body: some View {
+        content
+            .background(theme.color(.bgElevated))
+            .brutalBorder(width: borderWidth)
+            .hardShadow(offset: offset)
+    }
+}
