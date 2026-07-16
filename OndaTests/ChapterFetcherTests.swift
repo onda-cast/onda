0d6b98a461a5276c8313b4ea5c24b0467695e49d
Matthew Gilliam <chase.gilliam@gmail.com>
2026-07-16 15:28:18 -0400

    feat: ChapterFetcher with ad-marker detection; persist chaptersURL on Episode

---
+++ OndaTests/ChapterFetcherTests.swift
@@ -0,0 +1,16 @@
+//  ChapterFetcherTests.swift
+import XCTest
+@testable import Onda
+
+@MainActor
+final class ChapterFetcherTests: XCTestCase {
+    func test_decode_parsesChaptersAndFlagsAds() throws {
+        let url = Bundle(for: Self.self).url(forResource: "chapters", withExtension: "json")!
+        let chapters = ChapterFetcher().decode(try Data(contentsOf: url))
+        XCTAssertEqual(chapters.count, 4)
+        XCTAssertEqual(chapters[0].title, "Cold open")
+        XCTAssertEqual(chapters[2].startTime, 600)
+        XCTAssertTrue(chapters[2].isAd, "toc:false or 'sponsor' title marks an ad")
+        XCTAssertFalse(chapters[3].isAd)
+    }
+}
